8c2ca6a1c66925811fb6afd1a881fede80b94ce2
David Russell <ghostcode@me.com>
2019-11-03 20:54:21 -0500

    Update disabled state for default LED behavior

--- autotune/main.c
+++ autotune/main.c
@@ -1,9 +1,11 @@
-// PORTA CLOCK OUPUTS
+// PORTA CLOCK OUPUTS // Interrupt timing
 // 0: 112.640 Khz
 // 1: 440 Hz
 // 2: 220 Hz 1
 // 3: 220 Hz 2
 // 5: 27.5 Hz
+//
+// PORTB TUNE INPUT: 2
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -13,24 +15,32 @@
 ISR(TIM0_COMPA_vect, ISR_NAKED) {
   static uint16_t c = 0;
   static uint8_t output = 0;
-  output = ((c & 0x1) | ((c & 0x1300) >> 7)); 
-  output |= ((output ^ 0x4) & 0x4) << 1;
+
+  if (PINB & 0x4) {
+    output = ((c & 0x1) | ((c & 0x1300) >> 7)); 
+    output |= ((output ^ 0x4) & 0x4) << 1;
+  }
+  else {
+    output = 0xC;
+  }
+
   PORTA = output;
   c++;
   reti();
 }
 
 int main (void) {
-  DDRA |= 0x3F; // Set output pins
 
-  cli();
+  cli(); // Disable interrupts
+ 
+  DDRA |= 0x3F; // Set output pins
 
   // Setup timer interrupt
   TCCR0A = 0;
   TCCR0B = 0;
   TCNT0 = 0;  
 
-  OCR0A = (F_CPU / (INTERRUPT_FREQ));
+  OCR0A = (F_CPU / (INTERRUPT_FREQ)); // Interrupt timing
   TCCR0A |= (1 << WGM01); // CTC - Clear Timer on Compare
   TCCR0B |= (1 << CS00); // No prescaler
   TIMSK0 |= (1 << OCIE0A); // Compare A Match Interrupt Enable
